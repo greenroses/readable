4bd7d72d5e18399cc5d47c360e5dfcb91f81bd57
greenroses <xiaodan.zju@gmail.com>
2018-01-03 18:35:33 -0500

    add env api url to api.js for heroku deployment

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const api = 'http://localhost:3001'
+const api = process.env.REACT_APP_READABLE_API_URL || 'http://localhost:3001'
 const header = { headers: {'Authorization': 'whatever-you-want'}}
 
 export function getAllposts() {
